7b8a204116fbdfae5eba01b3abf2694b03ee38e6
CPA-PO170\Contraprova <mike.santos@contraprova.com.br>
2021-09-03 12:56:58 -0300

    Inclusão do tema do aplicativo e criação do header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -4,7 +4,18 @@ import style from './styles';
 export function Dashboard(){
   return(
     <style.Container>
-      <style.Title>Dashboard</style.Title>
+      <style.Header>
+        <style.UserWrapper>
+          <style.UserInfo>
+            <style.Photo source={{uri: 'https://avatars.githubusercontent.com/u/46286130?v=4'}} />
+            <style.User>
+              <style.UserGreeting>Olá,</style.UserGreeting>
+              <style.UserName>Mike</style.UserName>
+            </style.User>
+          </style.UserInfo>
+          <style.PowerIcon name="power-settings-new" />
+        </style.UserWrapper>
+      </style.Header>
     </style.Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,52 @@
 import styled from "styled-components/native";
+import {MaterialIcons} from '@expo/vector-icons';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export default {
   Container: styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
     background-color: ${({theme})=>theme.colors.background};
   `,
-  Title: styled.Text`
-    font-size: 24px;
-    font-weight: bold;
-    color:${({theme})=>theme.colors.title};
+  Header: styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    background-color: ${({theme})=>theme.colors.primary};
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  UserWrapper: styled.View`
+    width: 100%;
+    padding: 0 30px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  UserInfo: styled.View`
+    flex-direction: row;
+    align-items: center;
+  `,
+  Photo: styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 30px;
+  `,
+  User: styled.View`
+    margin-left: 17px;
+  `,
+  UserGreeting: styled.Text`
+    font-size: ${RFValue(18)}px;
+    color: ${({theme})=>theme.colors.shape};
+    font-family: ${({theme})=>theme.fonts.regular};
+  `,
+  UserName: styled.Text`
+    font-size: ${RFValue(18)}px;
+    color: ${({theme})=>theme.colors.shape};
+    font-family: ${({theme})=>theme.fonts.bold};
+    margin-top: -10px;
+  `,
+  PowerIcon: styled(MaterialIcons)`
+    color: ${({theme})=>theme.colors.secondary};
+    font-size: ${RFValue(24)}px;
   `,
 }
